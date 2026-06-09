de4d46a20ff0ee1bad2bdecbefe39bcc1259c0f8
C0A25120 <c0a251209b@edu.teu.ac.jp>
2026-06-09 15:25:33 +0900

    練習1と2: 背景画像の描画

---
+++ pygame_image.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600)) #サイズ
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg") #画像読み込み　画像Surfaceの生成
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0]) #作られたscreenのsurfaceの座標 0,0に貼り付け　貼り付けないと見えない。
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
