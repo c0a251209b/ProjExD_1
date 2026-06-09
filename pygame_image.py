364ba5d3f43951de2b30d50cdef3a8d405459811
C0A25120 <c0a251209b@edu.teu.ac.jp>
2026-06-09 15:58:23 +0900

    練習7,8,9:残像問題解決

--- pygame_image.py
+++ pygame_image.py
@@ -10,14 +10,18 @@ def main():
     screen = pg.display.set_mode((800, 600)) #サイズ
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #画像読み込み　画像Surfaceの生成
+    bg_img2 = pg.transform.flip(bg_img, True , False) #練習8 背景画像の左右反転
     kk_img = pg.image.load("fig/3.png") #練習3；こうかとんSurfaceの作成
-    kk_img = pg.transform.folip(kk_img, True , False) #練習３こうかとん左右反転
+    kk_img = pg.transform.flip(kk_img, True , False) #練習３こうかとん左右反転
+    
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr
+        x = tmr %3200  
         screen.blit(bg_img, [-x, 0]) #作られたscreenのsurfaceの座標 0,0に貼り付け　貼り付けないと見えない。
+        screen.blit(bg_img2, [-x+1600, 0])#練習7二枚目
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, [300, 200])        #練習4:こうかとんsurfaceの表示
         pg.display.update()
         tmr += 1
